b9efcabb27a66c9ec2b7dd30acb7839626956721
abdulhaseeb-user <haseebfaisal2002@gmail.com>
2024-04-26 10:47:49 +0500

    keylogging + mouse movement

--- Keylogger/keyanalyzer.py
+++ Keylogger/keyanalyzer.py
@@ -31,10 +31,10 @@ def analyze_keystrokes():
             end_time = timestamp
 
         # Count key combinations
-        if keystroke in key_combinations:
-            key_combinations[keystroke] += 1
+        if keystroke_str in key_combinations:
+            key_combinations[keystroke_str] += 1
         else:
-            key_combinations[keystroke] = 1
+            key_combinations[keystroke_str] = 1
 
     # Calculate total time elapsed
     time_elapsed = (end_time - start_time).total_seconds() / 60  # Convert to minutes
@@ -43,7 +43,7 @@ def analyze_keystrokes():
     average_typing_speed = total_keystrokes / time_elapsed
 
     # Analyze keystroke patterns
-    analyze_keystroke_patterns(keystroke_data, time_elapsed)
+    analyze_keystroke_patterns(keystroke_data)  # Remove the 'time_elapsed' argument
 
     return {
         "total_keystrokes": total_keystrokes,
@@ -53,40 +53,15 @@ def analyze_keystrokes():
     }
 
 
-def analyze_keystroke_patterns(keystroke_data, total_time_in_minutes):
+def analyze_keystroke_patterns(keystroke_data):
     # Calculate typing speed
-    typing_speed = len(keystroke_data) / total_time_in_minutes
-    print(keystroke_data)
-    # Example: Detecting high typing speed
-    threshold_typing_speed = 60  # Adjust threshold as needed
-    if typing_speed > threshold_typing_speed:
-        print("High typing speed detected! Potential cheating behavior.")
     
-    keystroke_str = ' '.join(keystroke_data)
-    print(keystroke_str)
+    keystroke_str = ' '.join([keystroke.split(": ")[1] for keystroke in keystroke_data])
     # Check for the sequence "Alt+Tab Ctrl+A Ctrl+C Alt+Tab Ctrl+V"
-    if "alt tab ctrl a ctrl c alt tab ctrl v" in keystroke_str:
+    sequence = "alt tab ctrl a ctrl c alt tab ctrl v"
+    if sequence in keystroke_str:
         print("Sequence 'Alt+Tab Ctrl+A Ctrl+C Alt+Tab Ctrl+V' detected! Potential cheating behavior.")
 
-    ctrl_v_count = 0
-    ctrl_z_count = 0
-
-    ctrl_pressed = False
-    for keystroke in keystroke_data:
-        if keystroke == "ctrl":
-            ctrl_pressed = True
-        elif ctrl_pressed and keystroke == "v":
-            ctrl_v_count += 1
-            if ctrl_v_count > 5:
-                print("Pasting excessively detected! Potential cheating behavior.")
-            ctrl_pressed = False
-        elif ctrl_pressed and keystroke == "z":
-            ctrl_z_count += 1
-            if ctrl_z_count > 5:
-                print("Undoing excessively detected! Potential cheating behavior.")
-            ctrl_pressed = False
-        else:
-            ctrl_pressed = False
     # Add more analysis logic as needed
 
 
