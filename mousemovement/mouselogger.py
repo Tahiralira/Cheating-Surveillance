935c043d589d63c681647462ccdca2d8dc56a863
TahirAlira <tahiralira3@gmail.com>
2024-04-28 08:25:39 +0500

    Mouse Logger Update (Tab Changes , Session Update in Log File, and Clipboard data logging done)

--- mousemovement/mouselogger.py
+++ mousemovement/mouselogger.py
@@ -10,8 +10,12 @@ def check_clipboard():
     while True:
         clipboard_content = pyperclip.paste()
         if clipboard_content != previous_clipboard_content:
-            print("New clipboard content:", clipboard_content)
+            #print("New clipboard content:", clipboard_content)
             previous_clipboard_content = clipboard_content
+            # Log the clipboard content
+            with open("windowtablogger.txt", "a") as log_file:
+                log_file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Clipboard: {clipboard_content}\n")
+        
         time.sleep(1)  # Adjust the polling interval as needed
 
 def log_active_window_change():
@@ -21,7 +25,7 @@ def log_active_window_change():
         active_window = gw.getActiveWindow()
         if active_window != previous_active_window:
             if active_window is not None:
-                print(f"Switched to: {active_window.title}")
+                #print(f"Switched to: {active_window.title}")
                 # Log the window title
                 with open("windowtablogger.txt", "a") as log_file:
                     log_file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Switched to: {active_window.title}\n")
@@ -29,6 +33,11 @@ def log_active_window_change():
         time.sleep(1)  # Adjust the polling interval as needed
 
 if __name__ == "__main__":
+    
+    # Add space and "New Session" heading to log file
+    with open("windowtablogger.txt", "a") as log_file:
+        log_file.write("\n\n\n\nNew Session Log\n\n")
+    
     # Start monitoring clipboard changes
     clipboard_thread = threading.Thread(target=check_clipboard)
     clipboard_thread.daemon = True
